b858edffe0b8df4bd5e1e6c0d1fbf316dc7695ea
Matt Glover <matt@duchysoftware.com>
2016-11-27 22:50:25 +0100

    Problem 0003 - became apparent when trying to get largest Prime Factor for 600851475143 that implementation had performance problems

--- ProjectEuler/AppDelegate.swift
+++ ProjectEuler/AppDelegate.swift
@@ -16,13 +16,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
+/// 	Problem 0001
 //		let problem0001 = Problem0001()
 //		let answer = problem0001.sumOfMultiples3And5Below(number: 1000)
 //		print("Problem 0001: \(answer)")
 
+/// 	Problem 0002
 //		let problem0002 = Problem0002()
 //		let answer = problem0002.sumOfEvenFibonacciNumbersLessThan(maxNumber: 4000000)
 //		print("Problem 0002: \(answer)")
+
+/// 	Problem 0003		
+		let problem0003 = Problem0003()
+		let number = 13195
+		guard let answer = problem0003.largestPrimeFactorFor(number: number) else {
+			print("No Prime Numbers for \(number)")
+			return true
+		}
+		print("Problem 0003: \(answer)")
+
 
 		return true
 	}
