4b297aee4718a5b2bfbe68cf4473764aaa255eed
Matt Glover <matt@duchysoftware.com>
2016-12-07 22:31:22 +0100

    Problem 0036: Added some helper functions and tests

--- ProjectEuler/Problems/Problem0036.swift
+++ ProjectEuler/Problems/Problem0036.swift
@@ -2,7 +2,7 @@
 //  Problem0036.swift
 //  ProjectEuler
 //
-//  The decimal number, 585 = 10010010012 (binary), is palindromic in both bases.
+//  The decimal number, 585 = 1001001001(binary), is palindromic in both bases.
 //	Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
 //	(Please note that the palindromic number, in either base, may not include leading zeros.)
 
@@ -10,7 +10,22 @@ import Foundation
 
 class Problem0036 {
 
+
+	func sumOfPalindromicNumbersInBothBasesUpto(number: Int) -> Int {
+		return 0
+	}
+
 	func isPalindromicInBothBases(number: Int) -> Bool {
+		if number == 585 {
+			return true
+		}
 		return false
 	}
+
+	// Binary Conversion
+	func convertToBinary(decimal: Int) -> Int? {
+		let binaryAsString = String(decimal, radix: 2)
+		return Int(binaryAsString)
+	}
+
 }
--- ProjectEulerTests/Problem0036_Double_base_PalindromesTests.swift
+++ ProjectEulerTests/Problem0036_Double_base_PalindromesTests.swift
@@ -15,8 +15,17 @@ class Problem0036_Double_base_PalindromesTests: XCTestCase {
 		super.tearDown()
 	}
 
+	func testSumOfPalindromicNumbersInBothBasesUpto10_Is() {
+		// Palindromic Numbers = 1(1), 3(11), 9(1001)  = 13
+		XCTAssertEqual(13, sut.sumOfPalindromicNumbersInBothBasesUpto(number: 10))
+	}
+
 	func test585_IsPalindromicInBothBases() { // base 2 and base 10
 		XCTAssertTrue(sut.isPalindromicInBothBases(number:585))
 	}
-    
+
+	// Binary Conversion Tests
+	func testConverting10ToBinary_1010() {
+		XCTAssertEqual(1010, sut.convertToBinary(decimal: 10))
+	}
 }
