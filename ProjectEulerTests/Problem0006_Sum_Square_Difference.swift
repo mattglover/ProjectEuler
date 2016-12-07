d93577adeee1ddcb345adfd38441de0f4a5cba6b
Matt Glover <matt@duchysoftware.com>
2016-12-07 20:54:30 +0100

    Problem0006: Added sanity test .. from:2 to:5

--- ProjectEulerTests/Problem0006_Sum_Square_Difference.swift
+++ ProjectEulerTests/Problem0006_Sum_Square_Difference.swift
@@ -18,4 +18,17 @@ class Problem0006_Sum_Square_Difference: XCTestCase {
 	func testSumSquareDifferenceOfNumbersFrom1To10_is2640() {
 		XCTAssertEqual(2640, sut.sumSquareDifferenceOfNumbers(from:1, to:10));
 	}
+
+	// Sanity test
+	func testSumSquareDifferenceOfNumbersFrom2To5_is142() {
+		// 2sq + 3sq + 4sq + 5sq
+		//  4  +  9  + 16  + 25 = 54
+
+		// 2 + 3 + 4 + 5 = 14
+		// 14sq = 196
+
+		// 196 - 54 = 142
+
+		XCTAssertEqual(142, sut.sumSquareDifferenceOfNumbers(from:2, to:5));
+	}
 }
